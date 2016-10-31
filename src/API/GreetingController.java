08a74fb2498fdb059f4bccbade667f99e16d59f0
anders11 <anderslatif@gmail.com>
2016-10-31 21:54:47 +0100

    Wrote lecture notes. Removed content of GreetingController

--- src/API/GreetingController.java
+++ src/API/GreetingController.java
@@ -1,22 +1,17 @@
 package API;
 
+
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.concurrent.atomic.AtomicLong;
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 @RestController
 public class GreetingController {
 
-    private static final String template = "Hello, %s!";
-    private final AtomicLong counter = new AtomicLong();
 
-    @RequestMapping("/greeting")
-    public String greeting(@RequestParam(value = "name"/*, defaultValue="World"*/) String name) {
 
-        System.out.println("Hi, from the console " + name);
 
-        return "It Works!!" + name;
-    }
 }
